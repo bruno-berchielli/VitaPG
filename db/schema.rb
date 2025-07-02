9ace9ec3a958d97bb381dacbc15e8fe20e035d4a
JohnnyPio <john.s.piotrowski@gmail.comwget>
2025-07-02 18:15:28 -0400

    Added sslmode option to the schema to prevent error

--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_05_145208) do
     t.string "database_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "sslmode"
   end
 
   create_table "destinations", force: :cascade do |t|
