02ed4e9405432cfa31ff896bbc50c853b7642150
JohnnyPio <john.s.piotrowski@gmail.comwget>
2025-07-09 19:15:09 -0400

    Define custom routes for the Google OAuth 2.0 flow. Rails will konw where to send users to initiate and complete the Google login/authorization process.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   mount MissionControl::Jobs::Engine, at: "/jobs"
-  mount Motor::Admin => '/admin'
+  mount Motor::Admin => "/admin"
+
+  get "/auth/google/start", to: "google_auth#start"
+  get "/auth/google/callback", to: "google_auth#callback"
 end
