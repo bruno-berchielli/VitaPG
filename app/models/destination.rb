2bc3a4b10fe8d53a1cb960683cb0952dd3abe0d0
JohnnyPio <john.s.piotrowski@gmail.comwget>
2025-07-11 17:35:50 -0400

    Whoops - forgot to commit this. Remove two subclasses from destination.

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -28,23 +28,3 @@ class Destination < ApplicationRecord
 
   validates :name, :provider, presence: true
 end
-
-class S3Destination < Destination
-  validates :bucket, :access_key_id, :secret_access_key, :region, presence: true
-  validates :client_id, :client_secret, :access_token, absence: true
-  validate :ensure_provider_is_s3
-
-  def ensure_provider_is_s3
-    errors.add(:provider, "must be 's3'") unless provider == "s3"
-  end
-end
-
-class GoogleDriveDestination < Destination
-  validates :client_id, :client_secret, :access_token, presence: true
-  validates :credentials_path, :bucket, :access_key_id, :secret_access_key, :region, absence: true
-  validate :ensure_provider_is_google_drive
-
-  def ensure_provider_is_google_drive
-    errors.add(:provider, "must be 'google_drive'") unless provider == "google_drive"
-  end
-end
